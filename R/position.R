174678703f45c1173dcd3f25c8a2fc619261d971
Jiri Lukavsky <jirilukavsky@users.noreply.github.com>
2018-11-06 16:55:19 +0100

    Added settings and default_settings

--- R/position.R
+++ R/position.R
@@ -58,3 +58,67 @@ is_distance_at_least <- function(position, min_distance) {
     dist()
   all(dist_triangle >= min_distance)
 }
+
+#' Default settings for position/trajectory code
+#'
+#' Returns a list of values used for position/trajectory generation or
+#' presentation. The values refer to the "physical properties"
+#' (e.g., definition of the objects or arena) or
+#' to the presentation propoerties
+#' (e.g., objects' colour, presence of border).
+#' The list is passed as a parameter to other functions.
+#'
+#' Currently used properties include:
+#' \describe{
+#'   \item{xlim}{A vector of form `c(xmin, xmax)`.
+#'   Represents x-limit of the arena square, objects are limited to this area.}
+#'   \item{ylim}{A vector of form `c(ymin, ymax)`.
+#'   Represents y-limit of the arena square, objects are limited to this area.}
+#'   \item{min_dist}{Minimum pairwise distance between centres of objects.}
+#'   \item{r}{Radius of the object. Object is considered to be a circle.}
+#'   \item{arena_border}{Logical. Whether arena border should be drawn.}
+#'   \item{fill_object}{Character. Colour code of default fill colour.}
+#'   \item{fill_target}{Character. Colour code of target fill colour.}
+#'   \item{border_object}{Character. Colour code of default border.}
+#'   \item{border_target}{Character. Colour code of target border.}
+#' }
+#'
+#' @return list of parameters
+#' @export
+#'
+#' @examples
+default_settings <- function() {
+  list(
+    xlim = c(-10, 10),
+    ylim = c(-10, 10),
+    min_dist = 1,
+    r = 0.5,
+    arena_border = T,
+    fill_object = "gray",
+    fill_target = "green",
+    border_object = "black",
+    border_target = "black"
+  )
+}
+
+
+#' Creates a custom copy of settings
+#'
+#' @param ... named list of properties to override default settings.
+#'
+#' @return
+#' @export
+#'
+#' @seealso \code{\link{default_settings}}
+#'
+#' @examples
+#' settings(xlim = c(0, 10), ylim = c(0, 10))
+settings <- function(...) {
+  settings_list <- default_settings()
+  dots <- list(...)
+  for (key in names(dots)) {
+    settings_list[[key]] <- dots[[key]]
+  }
+  settings_list
+}
+
