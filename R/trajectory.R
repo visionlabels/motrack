d23dd8abc7efbde014dadf1f4646bb6af7ce90cc
Jiri Lukavsky <jirilukavsky@users.noreply.github.com>
2018-11-29 18:54:29 +0100

    make_random_trajectory working
    Based on nested tibbles

--- R/trajectory.R
+++ R/trajectory.R
@@ -41,7 +41,7 @@ add_random_direction <- function(position) {
 
 make_random_trajectory <- function(start, timescale, settings, step_function) {
   # take start position
-  # are direction present? - add if not
+  # are direction/speed present? - add if not
   # make step
   stopifnot(length(timescale) > 1)
   if ("direction" %in% names(start)) {
@@ -54,19 +54,16 @@ make_random_trajectory <- function(start, timescale, settings, step_function) {
     moment <- moment %>% dplyr::mutate(speed = settings$speed)
   }
   moment <- moment %>% dplyr::mutate(time = timescale[1])
-  print(moment)
-  moment_list <- list()
-  moment_list[timescale[1]] <- moment
-  for (t in timescale[-1]) {
-    moment_next <- step_function(moment, t, settings)
-    moment_list[t] <- moment_next
+
+  moment_tbl <- tibble(time = timescale, position = list(tibble))
+  moment_tbl$position[[1]] <- moment
+  for (i in 2:length(timescale)) {
+    moment_next <- step_function(moment, timescale[i], settings)
+    moment_tbl$position[[i]] <- moment_next
     moment <- moment_next
   }
-  print(moment_list)
-  dplyr::bind_rows(moment_list)
+  moment_tbl %>% unnest()
 }
-make_random_trajectory(mom, 0:5, new_settings(speed = 1), step_direct)
-
 
 step_square_arena <- function(moment, time_next, settings) {
   # moment is position + direction + speed
@@ -88,4 +85,5 @@ step_direct <- function(moment, time_next, settings) {
 # pos <- generate_positions_random(8, default_settings(), border_distance = 3)
 # traj <- make_random_trajectory(mom, 0:5, new_settings(speed = 1), step_direct)
 # mom <- add_random_direction(pos) %>% mutate(speed = 1, time = 0)
+# aa=make_random_trajectory(mom, 0:5, new_settings(speed = 1), step_direct)
 # step_direct(mom, 1, default_settings())
