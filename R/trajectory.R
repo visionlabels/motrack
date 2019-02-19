3a498de926ef2f463f10aa6b7c93ac17b5a20871
Jiri Lukavsky <jirilukavsky@users.noreply.github.com>
2019-02-19 16:43:41 +0100

    Started zigzag trajectories

--- R/trajectory.R
+++ R/trajectory.R
@@ -60,6 +60,7 @@ add_random_direction <- function(position) {
 #' @param settings list with basic properties
 #' @param step_function function which is called in every time step
 #' to calculate next position in sequence
+#' @param ... Extra parameters passed to `step_function`
 #'
 #' @return Tibble trajectory object
 #' @export
@@ -73,7 +74,7 @@ add_random_direction <- function(position) {
 #' tt <- make_random_trajectory(
 #'   moment, seq(0, 8, by = 0.1), sett_move, step_direct
 #' )
-make_random_trajectory <- function(start, timescale, settings, step_function) {
+make_random_trajectory <- function(start, timescale, settings, step_function, ...) {
   # take start position
   # are direction/speed present? - add if not
   # make step
@@ -97,7 +98,7 @@ make_random_trajectory <- function(start, timescale, settings, step_function) {
   moment_tbl <- tibble::tibble(time = timescale, position = list(tibble::tibble))
   moment_tbl$position[[1]] <- moment
   for (i in 2:length(timescale)) {
-    moment_next <- step_function(moment, timescale[i], settings)
+    moment_next <- step_function(moment, timescale[i], settings, ...)
     moment_tbl$position[[i]] <- moment_next
     moment <- moment_next
   }
@@ -133,6 +134,10 @@ step_direct <- function(moment, time_next, settings) {
       y = .data$y + sin(.data$direction) * .data$speed * timestep,
       time = time_next)
   moment_next
+}
+
+step_zigzag <- function(moment, time_next, settings, ttb = c(.5, 1.5)) {
+
 }
 
 bounce_off_others <- function(moment, timestep, settings) {
